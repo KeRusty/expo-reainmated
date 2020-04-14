6cfb2a5c5cf07e8ddf9bc6b4770dc9f29a57b65a
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-14 13:03:24 +0530

    cards taken as component

---
+++ src/components/cards/cards-styles.js
@@ -0,0 +1,27 @@
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+    cardContainer: {
+        paddingHorizontal: 10,
+        paddingVertical: 4
+    },
+    contentContainer: {
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+        backgroundColor: '#ffffff',
+        flexDirection: 'row'
+    },
+    imageContainer: {
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        backgroundColor: '#000000'
+    },
+    textContainer: {
+        paddingHorizontal: 10,
+        justifyContent: 'center',
+        flexDirection: 'column'
+    }
+
+})
