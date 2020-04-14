b5d62f2ffdef1281e89f1af68de87bfacc9d0440
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-14 13:10:38 +0530

    Update home.js

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -15,6 +15,41 @@ export default function Home() {
                     subheading={"20 Reps"}
                 />
 
+                <Cards
+                    heading={"Long Plank"}
+                    subheading={"30 Seconds"}
+                />
+
+                <Cards
+                    heading={"Arm Sprint"}
+                    subheading={"30 Seconds"}
+                />
+
+                <Cards
+                    heading={"Back Step Lunge"}
+                    subheading={"20 Reps Each Leg"}
+                />
+
+                <Cards
+                    heading={"Shoulder Push Ups"}
+                    subheading={"15 Reps"}
+                />
+
+                <Cards
+                    heading={"Glute Leg Lifts"}
+                    subheading={"15 Reps Each Leg"}
+                />
+
+                <Cards
+                    heading={"Reverse Plank"}
+                    subheading={"30 Seconds"}
+                />
+
+                <Cards
+                    heading={"Step Ups"}
+                    subheading={"10 Reps Each Leg"}
+                />
+
             </ScrollView>
 
         </SafeAreaView>
