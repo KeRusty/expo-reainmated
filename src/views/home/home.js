0bc9d01730bc706ddc42337ceb811fcd6d110c2d
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-16 10:33:50 +0530

    small fixes

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { ScrollView, SafeAreaView } from 'react-native';
 
 import Cards from '../../components/cards/cards';
 import styles from './home-styles';
 
 export default function Home() {
+
     return (
         <SafeAreaView style={styles.container}>
 
