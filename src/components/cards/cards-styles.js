2b612aa45e4b8539b13c8f51985bb7d4a180a30e
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-04-16 11:11:50 +0530

    Update cards-styles.js

--- src/components/cards/cards-styles.js
+++ src/components/cards/cards-styles.js
@@ -20,7 +20,7 @@ export default StyleSheet.create({
         backgroundColor: '#ffffff',
         flexDirection: 'column',
         shadowColor: '#808080',
-        shadowOffset: { width: 8, height: 2 },
+        shadowOffset: { width: 8, height: 8 },
         shadowOpacity: 1,
         shadowRadius: 8,
         elevation: 4,
@@ -30,13 +30,21 @@ export default StyleSheet.create({
         paddingVertical: 20,
         paddingHorizontal: 20,
         borderRadius: 4,
-        backgroundColor: '#000000'
+        backgroundColor: '#000000',
     },
     pressedImageContainer: {
         backgroundColor: '#000000',
         width: '100%',
         borderRadius: 4,
         paddingVertical: 80,
+        alignItems: 'flex-end'
+    },
+    tickImage: {
+        height: 30,
+        width: 30,
+        bottom: 0,
+        marginRight: 10,
+        top: 70
     },
     textContainer: {
         paddingHorizontal: 10,
